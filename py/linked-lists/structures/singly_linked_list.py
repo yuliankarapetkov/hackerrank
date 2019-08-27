d0d8112bbd9dc5883f5534c1f23e8b4782be7ea5
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2019-08-27 13:55:18 +0300

    python singly linked list

---
+++ py/linked-lists/structures/singly_linked_list.py
@@ -0,0 +1,20 @@
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
