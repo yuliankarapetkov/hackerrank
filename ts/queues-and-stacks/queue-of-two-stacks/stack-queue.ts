30f668323b54be8ddca4896aa4b4927d48005e31
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2019-08-28 17:10:33 +0300

    queue of two stacks name refactoring

--- ts/queues-and-stacks/queue-of-two-stacks/stack-queue.ts
+++ ts/queues-and-stacks/queue-of-two-stacks/stack-queue.ts
@@ -1,29 +1,29 @@
 export class StackQueue {
-    private stack1: number[] = [];
-    private stack2: number[] = [];
+    private _stack1: number[] = [];
+    private _stack2: number[] = [];
 
     get size(): number {
-        return this.stack1.length + this.stack2.length;
+        return this._stack1.length + this._stack2.length;
     }
 
     get head(): number {
         this._shiftElements();
-        return this.stack2.length ? this.stack2[this.stack2.length - 1] : null;
+        return this._stack2.length ? this._stack2[this._stack2.length - 1] : null;
     }
 
     enqueue(data: number): void {
-        this.stack1.push(data);
+        this._stack1.push(data);
     }
 
     dequeue(): number {
         this._shiftElements();
-        return this.stack2.length ? this.stack2.pop() : null;
+        return this._stack2.length ? this._stack2.pop() : null;
     }
 
     private _shiftElements(): void {
-        if (!this.stack2.length) {
-            while (this.stack1.length) {
-                this.stack2.push(this.stack1.pop());
+        if (!this._stack2.length) {
+            while (this._stack1.length) {
+                this._stack2.push(this._stack1.pop());
             }
         }
     }
