0f8f973112071b104db3eb451c153f4d7ca7bfee
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2019-09-02 17:43:21 +0300

    selection sort

---
+++ ts/sorting/selection-sort/index.ts
@@ -0,0 +1,19 @@
+export function selectionSort(arr: number[]): number[] {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex !== i) {
+            const temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    return arr;
+}
